8e96268339d1e33636005265a53fcb98bd4ed95e
dimdimah <dimahluthfi15@gmail.com>
2024-02-07 19:08:19 +0700

    docs: adding readmefor installations

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -23,6 +23,7 @@
             </li>
         </ul>
     </div>
+
     <main class="container">
     </main>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"
